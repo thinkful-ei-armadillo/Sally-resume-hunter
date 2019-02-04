ac48343369feef8c590187b9630f7b4c2e96a07b
Hunter Kreshock <hkreshock@gmail.com>
2019-02-04 14:37:54 -0700

    added html and js

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// App
+function App() {
+    return (
+        <div className='container'>
+            <main role="main">
+                <script src="script.js"></script>
+                <h1> Sally Student</h1>
+                <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="photo of Sally Student" />
+                <h2> 555-555-5555  <a href="mailto:sally@student.edu">sally@student.edu</a>
+                </h2>
+                <h3> Education</h3>
+                <p>
+                    <h4>2016-2020 ASU</h4>
+                    Business/ Culinary
+                </p>
+                <h3>Employment</h3>
+                <h4>Masterchef || Chef 2014- present</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris egestas in sem luctus condimentum. Pellentesque faucibus felis eu ligula iaculis mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                <h4> Starbucks || Barista 2009-2014</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris egestas in sem luctus condimentum. Pellentesque faucibus felis eu ligula iaculis mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            </main>  
+            <footer role="contentinfo">2018 Sally Student</footer>
+        </div>
+    );
+  };
+
+  // Use App:
+  // Insert the App's element tree into the #application-root
+  // ReactDOM.render(<App></App>, document.querySelector('#application-root'));
